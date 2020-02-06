4338038dd7359c7c737518c7bbefc25e13daf565
misabiko <misabiko@gmail.com>
2020-02-05 20:10:47 -0500

    Added basic damping switch on jump

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -1,15 +1,20 @@
-﻿using Cinemachine;
+﻿using System;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour {
 	public CinemachineFreeLook cam;
 	public PlayerInput playerInput;
+	public CharacterController characterController;
 	
 	public float camXSensitivity;
 	public float camYSensitivity;
 
+	public float upThreshold = 0.1f;
+
 	Vector2 lookInput;
+	bool lastGoingUp = false;
 
 	void Start() {
 		playerInput.actions["Look"].started += OnLook;
@@ -22,5 +27,21 @@ public class CameraMovement : MonoBehaviour {
 	void Update() {
 		cam.m_XAxis.Value += camXSensitivity * lookInput.x;
 		cam.m_YAxis.Value -= camYSensitivity * lookInput.y;
+	}
+
+	void LateUpdate() {
+		bool goingUp = characterController.velocity.y > upThreshold;
+
+		if (goingUp == lastGoingUp) return;
+		lastGoingUp = !lastGoingUp;
+		
+		float damping = goingUp ? 0.1f : 1f;
+
+		for (int i = 0; i < 3; i++) {
+			CinemachineTransposer transposer = cam.GetRig(i).GetCinemachineComponent<CinemachineTransposer>();
+			transposer.m_XDamping = damping;
+			transposer.m_YDamping = damping;
+			transposer.m_ZDamping = damping;
+		}
 	}
 }
