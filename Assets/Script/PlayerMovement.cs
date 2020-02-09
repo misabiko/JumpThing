0ba7d18b8af643a39f6cc8559a6c38119429e186
misabiko <misabiko@gmail.com>
2020-02-08 19:54:48 -0500

    Changed and tweaked animations + Variable run speed

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,18 +1,27 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
 	public Transform camTransform;
-	public float maxSpeed;
+	public float maxSpeedJog;
+	public float maxSpeedRun;
+	public float runThreshold;	//Must match animator's transition conditions
 	public float jumpForce;
 	public float gravityScale;
 	public float turnSpeed;
+	public Animator anim;
 
 	CharacterController characterController;
 	Vector2 moveInput;
 	Vector3 moveDirection;
 	Vector3 verticalVel;
-	
+
+	public TextMeshProUGUI velText;
+	static readonly int MoveInputMagnitude = Animator.StringToHash("MoveInputMagnitude");
+	static readonly int Jump = Animator.StringToHash("Jump");
+	static readonly int AirBorn = Animator.StringToHash("AirBorn");
+
 	void Awake() {
 		characterController = GetComponent<CharacterController>();
 
@@ -34,8 +43,11 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void OnJump(InputAction.CallbackContext context) {
-		if (characterController.isGrounded)
-			verticalVel.y = jumpForce;
+		if (!characterController.isGrounded) return;
+		
+		verticalVel.y = jumpForce;
+		anim.SetTrigger(Jump);
+		anim.SetBool(AirBorn, true);
 	}
 
 	void Update() {
@@ -44,13 +56,24 @@ public class PlayerMovement : MonoBehaviour {
 		camForward.Normalize();
 		
 		moveDirection = camForward * moveInput.y + camTransform.right * moveInput.x;
+		anim.SetFloat(MoveInputMagnitude, moveDirection.magnitude);
 	}
 
 	void FixedUpdate() {
 		verticalVel += Physics.gravity * gravityScale * Time.fixedDeltaTime;
-		Vector3 velocity = moveDirection * maxSpeed + verticalVel;
 
+		Vector3 velocity;
+		if (moveDirection.sqrMagnitude <= runThreshold * runThreshold)
+			velocity = moveDirection * maxSpeedJog;
+		else {
+			Vector3 direction = moveDirection.normalized;
+			velocity = (moveDirection - direction * runThreshold) * maxSpeedRun + direction * runThreshold * maxSpeedJog;
+		}
+		velText.text = "Input: " + (Mathf.Round(moveDirection.magnitude * 100f) / 100f) + "\nVel: " + (Mathf.Round(velocity.magnitude * 100f) / 100f);
+		velocity += verticalVel;
+		
 		characterController.Move(velocity * Time.fixedDeltaTime);
+		
 		if (moveDirection != Vector3.zero) {
 			Quaternion goalRot = Quaternion.LookRotation(moveDirection);
 			Quaternion slerp = Quaternion.Slerp(transform.rotation, goalRot, turnSpeed * moveDirection.magnitude * Time.fixedDeltaTime);
@@ -58,7 +81,9 @@ public class PlayerMovement : MonoBehaviour {
 			transform.rotation = slerp;
 		}
 
-		if (characterController.isGrounded)
+		if (characterController.isGrounded) {
 			verticalVel = Vector3.zero;
+			anim.SetBool(AirBorn, false);
+		}
 	}
 }
